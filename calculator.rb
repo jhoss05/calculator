9cb576631a1e8b261f21a54d877458d84bfddd37
Justin Hosman <jhoss05@gmail.com>
2014-09-17 15:22:09 -0600

    Changed Calculator so that it could handle decimals regardless of the operator and provided operator option at beginning for easier exit access.

--- calculator.rb
+++ calculator.rb
@@ -3,30 +3,47 @@ def say(words)
 end
 
 loop do 
+  say "What would you like to do?"
+  puts "- - - - - - - - - - - - - - -"
+  say "1) Add, 2) Subtract, 3) Multiply, 4) Divide, 5) Shut Down Calculator"
+  operator = gets.chomp
+  if operator == "5"
+    puts "- - - - - - - - - - - - - - -"
+    puts "You chose #{operator}"
+    puts "- - - - - - - - - - - - - - -"
+    puts "Calculator shutting down..."
+    puts "- - - - - - - - - - - - - - -"
+    puts "Calculator shut down."
+    puts "- - - - - - - - - - - - - - -"
+    break
+  end
+
+  puts "You chose operator #{operator}."
+  puts "- - - - - - - - - - - - - - -"
+
+
   say "Give me a number:"
   num_1 = gets.chomp
 
   say "Give me your second number:"
   num_2 = gets.chomp
 
-  say "What would you like to do?"
-  say "1) Add, 2) Subtract, 3) Multiply, 4) Divide, 5) Shut Down Calculator"
-  operator = gets.chomp
-
-  puts "You chose #{operator}"
-
   if operator == "1"
-    result = num_1.to_i + num_2.to_i
-    say "The result is #{result}."
+    result = num_1.to_f + num_2.to_f
+    say "The result is #{result}"
+    puts "- - - - - - - - - - - - - - -"
   elsif operator == "2"
-    result = num_1.to_i - num_2.to_i
-    say "The result is #{result}."
+    result = num_1.to_f - num_2.to_f
+    say "The result is #{result}"
+    puts "- - - - - - - f- - - - - - - -"
   elsif operator == "3"
-    result = num_1.to_i * num_2.to_i
-    say "The result is #{result}."
+    result = num_1.to_f * num_2.to_f
+    say "The result is #{result}"
+    puts "- - - - - - - - - - - - - - -"
   elsif operator == "4"
     result = num_1.to_f / num_2.to_f
-    say "The result is #{result}."
+    say "The result is #{result}"
+    puts "- - - - - - - - - - - - - - -"
   elsif operator == "5"
     say "Calculator shutting down..."
     say "Calculator shut down."
